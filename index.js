8dec5d74d4d9801834448d30c4ebc4befd7b2fca
cristianfernandezcabre <32138165+cristianfernandezcabre@users.noreply.github.com>
2018-02-16 01:44:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) =>{
+	if (!message.content.startsWith(prefix)) return;
+	  if (message.author.bot) return;
+
+	  if (!points[message.author.id]) points[message.author.id] = {
+	    points: 0,
+	    level: 0
+	  };
+	  let userData = points[message.author.id];
+	  userData.points++;
+
+	  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+	  if (curLevel > userData.level) {
+	    // Level up!
+	    userData.level = curLevel;
+	    message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
+	  }
+
+	  if (message.content.startsWith(prefix + "level")) {
+	    message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+	  }
+	  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+	    if (err) console.error(err)
+	  });
+	
 	if (message.content == '!jordi') {
 		var rand = Math.round(Math.random()*(1));
 		var msg;
